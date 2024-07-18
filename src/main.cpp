4a35f1443763d4e5e0ed66ea3026f9891328c073
SadanoRapa <gabriele.callegaro@gmail.com>
2024-07-18 02:34:50 +0200

    Laying down main structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,91 @@
 #include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <chrono>
+
+#include <glm/glm.hpp> // vec2, vec3, mat4, radians
+
+using namespace std::chrono_literals;
+
+constexpr int g_width = 1280;
+constexpr int g_height = 720;
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode(g_width, g_height), "My window", sf::Style::Close );
+    window.setFramerateLimit(60);
+
+    std::string map;
+    map += {'w','w','w','w','w','w','w','w'};
+    map += {'w',' ',' ',' ',' ',' ',' ','w'};
+    map += {'w',' ',' ',' ',' ',' ',' ','w'};
+    map += {'w',' ',' ',' ',' ',' ',' ','w'};
+    map += {'w',' ',' ',' ',' ',' ',' ','w'};
+    map += {'w','w','w','w','w','w','w','w'};
+
+    constexpr int mapX = 8;
+    constexpr int mapY = 6;
+
+    std::chrono::nanoseconds dt(0ns);
+    int frameCounter = 0;
+    auto tStart = std::chrono::high_resolution_clock::now();
+
+    sf::CircleShape shape(5.f);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        auto deltaTime = std::chrono::high_resolution_clock::now() - tStart;
+        tStart = std::chrono::high_resolution_clock::now();
+        dt += deltaTime;
+        frameCounter++; 
+        
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            switch (event.type)
             {
-                window.close();
+                case sf::Event::Closed:
+                    window.close();
+                    break;
             }
         }
 
-        window.clear();
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+
+        
+
+        sf::Color sCol = shape.getFillColor();
+        for (int i = 0; i < 100; i++)
+        {
+            shape.setPosition(10*i, 0);
+            shape.setFillColor({ static_cast<sf::Uint8>(dt.count()), static_cast<sf::Uint8>(dt.count() * 1), static_cast<sf::Uint8>(dt.count() * 2)});
+            window.draw(shape);
+        }
+        for (int i = 0; i < 100; i++)
+        {
+            shape.setPosition(10 * i, 10);
+            shape.setFillColor({ static_cast<sf::Uint8>(dt.count()), static_cast<sf::Uint8>(dt.count() * 1), static_cast<sf::Uint8>(dt.count() * 2)});
+            window.draw(shape);
+        }
+
+        auto tEnd = std::chrono::steady_clock::now();
+        deltaTime = tEnd - tStart;
+
+        
+        
+        if (dt > 1s)
+        {
+            std::cout << frameCounter << std::endl;
+            dt = 0ms;
+            frameCounter = 0;
+        }
+
+        // end the current frame
         window.display();
     }
+
+    return 0;
 }
