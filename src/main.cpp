83d770e778978cca25eabcb336f10d543ecaa1df
SadanoRapa <gabriele.callegaro@gmail.com>
2024-07-20 05:13:00 +0200

    working map and fake 3d

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ constexpr int g_height = 720;
 
 inline void handle_events(sf::Window&, const GameCore::PlayerControler&);
 
-void draw_on_screen(sf::Uint8*);
+void draw_on_screen(sf::RenderWindow& window, const std::vector<RayInfo>& points, sf::Uint8* pixels);
 void draw_on_screen(sf::RenderWindow&, const std::vector<RayInfo>& ,const sf::Vector2f& );
 
 int main()
@@ -22,12 +22,12 @@ int main()
     window.setFramerateLimit(0);
 
     //create canvas
-    //sf::Uint8* pixels = new sf::Uint8[g_width * g_height * 4];
-    //sf::Texture viewTexture;
-    //viewTexture.create(g_width, g_height);
-    //sf::Sprite view(viewTexture);
+    sf::Uint8* pixels = new sf::Uint8[g_width * g_height * 4];
+    sf::Texture viewTexture;
+    viewTexture.create(g_width, g_height);
+    sf::Sprite view(viewTexture);
 
-    GameCore gameCore{ {{g_width, g_height}, 0.25f * (glm::pi<float>()), 30.f, 0.02f} };
+    GameCore gameCore{ {{g_width, g_height}, 0.25f * (glm::pi<float>()), 8.f*8, 0.02f}};
 
     if (!gameCore.load_map("map.txt"))
     {
@@ -36,12 +36,13 @@ int main()
     }
 
     //sf::Vector2f plPos{ 15.f, 10.f };
-    sf::Vector2f plPos{ 4.5f, 2.f };
     std::vector<RayInfo> rayInfoVec;
 
     GameCore::PlayerControler playerController(&gameCore);
 
     gameCore.start_internal_time();
+
+    debug::GameTimer gt;
 
     while (window.isOpen())
     {
@@ -49,20 +50,23 @@ int main()
         handle_events(window, playerController);
         window.clear(sf::Color::Black);
 
-        debug::GameTimer gt;
+        
 
         gt.reset_timer();
         
         gameCore.update_entities();
         rayInfoVec = gameCore.view_by_ray_casting();
 
-        //std::cout << "casting" << '\t' << gt.reset_timer() << std::endl;
+        std::cout << "casting" << '\t' << gt.reset_timer() << std::endl;
 
-        draw_on_screen(window, rayInfoVec, plPos);
+        
+        draw_on_screen(window, rayInfoVec, pixels);
+        viewTexture.update(pixels);
+        window.draw(view);
 
-        //std::cout << "drawing" << '\t' << gt.reset_timer() << std::endl;
-        //viewTexture.update(pixels);
-        //window.draw(view);
+        draw_on_screen(window, rayInfoVec, { g_width - 4 * 32,4 * 32 });
+
+        std::cout << "drawing" << '\t' << gt.reset_timer() << std::endl;
         window.display();
 
         gt.add_frame();
@@ -113,28 +117,44 @@ inline void handle_events(sf::Window& window, const GameCore::PlayerControler& p
 
 }
 
-void draw_on_screen(sf::RenderWindow& window, const std::vector<RayInfo>& points, const sf::Vector2f& start)
+void draw_on_screen(sf::RenderWindow& window, const std::vector<RayInfo>& raysInfo, const sf::Vector2f& start)
 {
     
-    sf::VertexArray lines(sf::Lines, points.size() * 2);
-    float multip = 100;
-    for (int i = 0; i < points.size(); ++i)
+    sf::VertexArray lines(sf::Lines, raysInfo.size() * 2);
+    float multip = 32;
+    for (int i = 0; i < raysInfo.size(); ++i)
     {
-        lines[i * 2] = sf::Vector2f(start.x * multip, start.y * multip) ;
-        lines[i * 2 + 1] = { { (start.x + points[i].hitPos[0]) * multip, (start.y + points[i].hitPos[1]) * multip}, sf::Color(i%256, 0xFF, 0xFF) };
+        lines[i * 2] = sf::Vector2f(start.x, start.y ) ;
+        lines[i * 2 + 1] = { { start.x + raysInfo[i].hitPos[0] * multip, start.y + raysInfo[i].hitPos[1] * multip}, sf::Color(i%256, 0xFF, 0xFF) };
         //std::cout << static_cast<char>(r.entityHit) << " " << r.hitPos[0] << " " << r.hitPos[1] << std::endl;
 
     }
     window.draw(lines);
 }
 
-void draw_on_screen(sf::RenderWindow& window, const std::vector<RayInfo>& points, sf::Uint8* pixels)
+void draw_on_screen(sf::RenderWindow& window, const std::vector<RayInfo>& raysInfo, sf::Uint8* pixels)
 {
-    for (int i = 0; i < g_width * g_height * 4; i += 4) 
+    for (int i = 0; i < raysInfo.size(); ++i)
     {
-        pixels[i] = 0xff;
-        pixels[i + 1] = i%120;
-        pixels[i + 2] = i%200;
-        pixels[i + 3] = i%256;
+ 
+        if (raysInfo[i].entityHit == EntityType::Wall) 
+        {
+            float distance = glm::length(raysInfo[i].hitPos) + 1;
+            float distanceFromFloor = (g_height * (1 - 1/distance))*0.5f;
+
+            sf::Color pixelColor;
+            for (int y = 0; y < g_height * 4; y+=4)
+            {
+                int x = i * 4;
+                if (y < distanceFromFloor*4 || y > (g_height - distanceFromFloor)*4)
+                    pixelColor = { 0x32, 0, 0x32, 0xff };
+                else 
+                    pixelColor = { 0xff, 0xff, 0xff , 0xff };
+                pixels[y * g_width + x] = pixelColor.r;
+                pixels[y * g_width + x + 1] = pixelColor.g;
+                pixels[y * g_width + x + 2] = pixelColor.b;
+                pixels[y * g_width + x + 3] = pixelColor.a;
+            }
+        }
     }
 }
